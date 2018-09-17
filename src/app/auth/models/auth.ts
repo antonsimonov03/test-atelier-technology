ed5c5445497daaae976b6a461e97da34eb6fe74d
antonsimonov03 <antonsimonov03@gmail.com>
2018-09-17 17:46:38 +0300

    Initial commit

---
+++ src/app/auth/models/auth.ts
@@ -0,0 +1,15 @@
+export class LoginRequest {
+    grant_type: string;
+    username: string;
+    password: string;
+    scope: string;
+    resource: string;
+}
+
+export class AuthResponse {
+    access_token: string;
+    expires_in: number;
+    id_token: string;
+    refresh_token: string;
+    token_type: string;
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  authApi: 'https://id.pe.atelierclient.com',
+  api: 'https://pe.atelierclient.com/api'
+};
